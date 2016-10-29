fccd95332331f43a6b5a3a715018c55952684da6
Nikki K <808nikz@gmail.com>
2016-10-28 20:03:52 -1000

    Setting up the basic server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.send('index.html');
+});
+
+app.use('/404', function (req, res) {
+  res.status(404).render('404');
+});
+
+const server = app.listen(PORT, function () {
+  console.log('Now listening on PORT:' + PORT);
+});
