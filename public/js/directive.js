f0758d308b6054318cc1e11d09ea11b3e973e716
Nikki K <808nikz@gmail.com>
2016-10-30 20:40:44 -1000

    When the user scrollls the nav bar will change

---
+++ public/js/directive.js
@@ -0,0 +1,18 @@
+angular.module('myApp', [])
+  .directive("navScroll",
+    function ($window) {
+      return function (scope, element, attr) {
+        angular.element($window).bind("scroll", function () {
+          if (!scope.scrollPosition) {
+            scope.scrollPosition = 0;
+          } if (this.pageYOffset > scope.scrollPosition) {
+            scope.boolChangeClass = true;
+          } else {
+            scope.boolChangeClass = false;
+          }
+          scope.scrollPosition = this.pageYOffset;
+          scope.$apply();
+        })
+      }
+    }
+  )
