c90aab33a8ab6755995b63da441f75a5eb2347dd
Nikki K <808nikz@gmail.com>
2016-10-30 18:56:05 -1000

    Removing the  function

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -2,24 +2,26 @@ angular.module('myApp', ['ngRoute'])
   .config([
     '$routeProvider',
     '$locationProvider',
-    function ($routerProvider, $locationProvider) {
+    function ($routeProvider, $locationProvider){
       $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
       });
 
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/default.html'
-      })
-      .when('/work', {
-        templateUrl: 'views/work.html'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html'
-      })
-      .when('/404', {
-        templateUrl: 'views/404.html'
-      })
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/default.html'
+        })
+        .when('/about', {
+          templateUrl: 'views/about.html',
+          controller: 'booksController'
+        })
+        .when('/work', {
+          templateUrl: 'views/work.html',
+          controller: 'moviesController'
+        })
+        .when('/404', {
+          templateUrl: 'views/404.html'
+        });
     }
   ])
