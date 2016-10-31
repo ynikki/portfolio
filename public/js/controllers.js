f245347aff3450315a794f302e8adc5214e7fbb2
Nikki K <808nikz@gmail.com>
2016-10-30 18:55:30 -1000

    creating controllers for the about and work

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,23 @@
-angular.module('myApp', ['ngRoute'])
-  .controller('myController', function ($scope, $route, $routeParams, $location) {
+angular.module('myApp')
+  .controller('mainController', [
+    '$scope',
+    '$route',
+    '$routeParams',
+    '$location',
+    function ($scope, $route, $routeParams, $location) {
     $scope.$route = $route;
     $scope.$location = $location;
     $scope.$routeParams = $routeParams;
-  })
+  }]).controller('booksController', [
+      '$scope',
+      '$routeParams',
+      function ($scope, $routeParams) {
+        $scope.params = $routeParams;
+      }
+  ]).controller('moviesController', [
+      '$scope',
+      '$routeParams',
+      function ($scope, $routeParams) {
+         $scope.params = $routeParams;
+      }
+  ]);
